0a376828f8be8cca74937527b730ea2e8d748d19
alex <aleksey.shilkin@fabit.ru>
2020-09-30 23:30:55 +0300

    Added yandex map

--- sample/src/main/java/ru/ashilkin/sample/MainActivity.kt
+++ sample/src/main/java/ru/ashilkin/sample/MainActivity.kt
@@ -2,27 +2,40 @@ package ru.ashilkin.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val MAPKIT_API_KEY = "d282c3a3-acd5-43b9-a66e-a00a53640272"
-    private val TARGET_LOCATION: Point = Point(59.945933, 30.320045)
+    private val MAPKIT_API_KEY = ""
+    private val TARGET_LOCATION: Point = Point(50.35848, 36.353309)
 
 
     var i = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+        MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
+
+        super.onCreate(savedInstanceState)
+
+        mapview.getMap().move(
+            CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 5f),
+            null
+        )
+
         mercatorView.setZOrderOnTop(true)
+
         button.setOnClickListener {
             i++
             text.text = "Click count = $i"
         }
-        mercatorView
     }
 
     override fun onPause() {
@@ -34,5 +47,18 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         mercatorView.onResume()
     }
+
+    override fun onStop() {
+        mapview.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapview.onStart()
+    }
+
 
 }
